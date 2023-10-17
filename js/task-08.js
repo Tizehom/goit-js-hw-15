ba03076bb7d608a3392b599d051d86d23dba9b0b
Maks <115037283+Tizehom@users.noreply.github.com>
2023-10-16 19:44:43 -0600

    3

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+
+  const email = elements.email.value;
+  const password = elements.password.value;
+
+  if (email.trim() === "" || password.trim() === "") {
+    alert("All fields must be completed!");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
